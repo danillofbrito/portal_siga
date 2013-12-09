baa2c87ac71d978664dc3211674bb95d0924ec1e
dfranca <dfranca@ciandt.com>
2013-12-09 21:37:26 -0200

    Uploading files

--- sites/all/themes/peak/templates/views-view-fields--latest-post.tpl.php
+++ sites/all/themes/peak/templates/views-view-fields--latest-post.tpl.php
@@ -1,25 +1,25 @@
 <?php
 /*
 ------------------------------------------------------------
-    Peak for Drupal 7.x - Version 1.1                           
-    Copyright (C) 2012 esors.com All Rights Reserved.           
-    @license - Copyrighted Commercial Software                   
+    Peak for Drupal 7.x - Version 1.1
+    Copyright (C) 2012 esors.com All Rights Reserved.
+    @license - Copyrighted Commercial Software
 ------------------------------------------------------------
-    Theme Name: Peak                                            
-    Author: ESORS                                           
-    Date: 25th July 2012                                        
-    Website: http://www.esors.com/                              
+    Theme Name: Peak
+    Author: ESORS
+    Date: 25th July 2012
+    Website: http://www.esors.com/
 ------------------------------------------------------------
-    This file may not be redistributed in whole or   
-    significant part.                                            
-------------------------------------------------------------*/   
+    This file may not be redistributed in whole or
+    significant part.
+------------------------------------------------------------*/
 ?>
 
 <span class="date"><i><?php print $fields['created']->content; ?></i></span>
 <h3 class="title"><a href=""><?php print $fields['title']->content; ?></a></h3>
 <div class="content">
   <div class="content-inner">
-    <?php print $fields['field_image']->content; ?>
+    <?php print $fields['field_foto']->content; ?>
     <?php print $fields['body']->content; ?>
   </div>
   <div class="read-more"><?php print $fields['view_node']->content; ?></div>
