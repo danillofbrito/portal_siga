37bad4cab19c1f679dd8c435ab203b2e70019bfa
dfranca <dfranca@ciandt.com>
2013-12-16 23:41:39 -0200

    Updating code and database.

---
+++ sites/default/files/js/js_c2gxw6VCDHdF1w1AV18Htgsd4Z8N68w8rR1OQl6JzS0.js
@@ -0,0 +1,37 @@
+(function ($) {
+
+  $(document).ready(function () {
+    $('.field-name-field-cupom img').click(function() {
+      $(this).jqprint();
+    });
+
+    if ($('body.node-type-anuncios').length > 0) {
+      var cupons = $('.field-name-field-cupom .field-item').size();
+
+
+      if (cupons == 2) {
+        // Hide tab 2
+        $('.field-name-field-cupom .field-item').last().hide();
+        $('.field-name-field-cupom .field-items').prepend("<a href='javascript:void(0)' class='cupom-tab cupom-2-tab'>Cupom 2</a>");
+        $('.field-name-field-cupom .field-items').prepend("<a href='javascript:void(0)' class='cupom-tab cupom-1-tab active'>Cupom 1</a>");
+
+        $('a.cupom-1-tab').click(function(){
+          $('.field-name-field-cupom .field-item').first().show();
+          $('.field-name-field-cupom .field-item').last().hide();
+          $(this).addClass('active');
+          $('a.cupom-2-tab').removeClass('active');
+        });
+
+        $('a.cupom-2-tab').click(function(){
+          $('.field-name-field-cupom .field-item').first().hide();
+          $('.field-name-field-cupom .field-item').last().show();
+          $(this).addClass('active');
+          $('a.cupom-1-tab').removeClass('active');
+        });
+      }
+
+    }
+  });
+
+})(jQuery);
+;
